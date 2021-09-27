290cceb93ba46f7316e43052b0259e79b5ffaa02
AgusQuintanar <agusquintanar17@gmail.com>
2021-09-27 05:17:22 -0500

    adds express server files

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,24 @@
+'use strict';
+
 const express = require("express");
 const cors = require("cors");
+const config = require("./config");
+const patientRoutes = require("./routes/patient-routes");
+const doctorRoutes = require("./routes/doctor-routes");
+const appointmentRoutes = require("./routes/appointment-routes");
+const fileUpload = require('express-fileupload');
+
 
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true }))
+app.use(fileUpload());
 
 
-//* --------- Controllers -----------
+app.use('/api', patientRoutes.routes, doctorRoutes.routes, appointmentRoutes.routes);
 
-
-const PORT = process.env.PORT || 3000
-app.listen( PORT, () => {
-    console.log("Corriendo en el puerto " + PORT);
-});
+app.listen(config.port, () => console.log('App is listening on url http://localhost:' + config.port));
